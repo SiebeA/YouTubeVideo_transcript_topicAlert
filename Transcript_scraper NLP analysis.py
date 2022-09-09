484430eb9ebf389f5cce2584a5cde1f138f72ce8
Siebe albers <savdelz@gmail.com>
2022-09-09 09:50:35 +0200

    reformat

--- Transcript_scraper NLP analysis.py
+++ Transcript_scraper NLP analysis.py
@@ -1,9 +1,16 @@
     
-import os, glob, pickle
-import pandas as pd
+
+wordOfInterest = 'naval'
+chars_of_Context = 200
+
+
+#%%=======================#
+'  Importing transcripts-sets              '
+#========================= #
+import os, glob
+# import pandas as pd
 import re
-from collections import Counter
-import spacy
+# from collections import Counter
 # import spacy
 """
     - Also return the surrounding words of the match
@@ -27,12 +34,11 @@ del f, file
 
 
 
+
 #%%=======================#
 '            REGEX return episode where a key word was mentioned:                 '
 #========================= #
 
-wordOfInterest = '1984'
-chars_of_Context = 200
 
 
 # MATCH the Title etc of the video in which the pattern occurs
@@ -44,47 +50,62 @@ a_matches = re.findall(regex, a_strings_transcripts) #it returns the date and ti
 for i in a_matches: print(i,'\n')
 
 
+# OPTINAL: word statistics
+words = re.findall('\w+', a_strings_transcripts)
+terms = set(words)
+
+
 # MATCH the context of the patterns
 wordOfInterestψContext = f"(.{chars_of_Context})({wordOfInterest})(\s.{chars_of_Context})"# matches X chars before and after the word of interest
 wordOfInterestψContext = re.sub('\.(\d+)\)', r'.{\1})', wordOfInterestψContext) # substituting/adding the curly brackets (in)
 
 aa_matches = re.findall(wordOfInterestψContext, a_strings_transcripts)
-for match in aa_matches: print(match,'\n\n')
+# it APPENDS, so delete file after use, or find a way to not append duplicate lines
+with open("output.txt", "a") as myfile:
+    for match in aa_matches:
+        print(match,'\n\n')
+        # Create 
+        myfile.write(match[0]+match[1]+match[2] + "\n\n") # create 1 string per match, + newLine
+        
 # matching a group of the pattern; ie context before, the word, or context after ; each is parenthesized in the {wordOfInterestψContext} variable. 
 
 
-# =============================================================================
-#  NER counter
-# =============================================================================
 
-    #match the paragraphs with corresponding ID
-regex = r"(\d\n\d+-\d+-\d+\n)(.+)\n(.+\n\n)(.+)"
-matches = re.finditer(regex, a_strings_transcripts, re.MULTILINE) #beware of generator : needs to be reinitated after use
-a_matchess = [(match.group(2),match.group(4))  for match in matches] #match ID and text
-#convert to dic:
-dict={}
-for a,b in a_matchess:
-    dict.setdefault(a, []).append(b)
-del a,b
-# explanation: how is it sorted? ; date chronological, despite when you look in the variable explorer (when it's sorted alphabetically)
 
-    # nlping the latest transcript (ie index 0)
 
-nlp = spacy.load("en_core_web_sm")
-doc = nlp(dict[list(dict.keys())[0]][0])
 
-    #NER summarizing
-labels, Counter = [ent.label_ for ent in doc.ents], 
-Counter(labels)
-items = [ent.text for ent in doc.ents if not (ent.label_ =='CARDINAL')]
-a_nerCounter = pd.DataFrame(Counter(items).most_common(10), columns =['ner', 'occurence']) #1st argument = list of tuples with a coupleTuple
-    #now scale to all the transcripts:
-listOf_a_nerCounter = []
-for key in list(dict.keys())[:5]:
-    doc = nlp(dict[key][0])
-    labels = [ent.label_ for ent in doc.ents]
-    Counter(labels)
-    items = [ent.text for ent in doc.ents]
-    a_nerCounter = pd.DataFrame(Counter(items).most_common(10), columns =['ner', 'occurence']) #1st argument =
-    listOf_a_nerCounter.append((key, pd.DataFrame(Counter(items).most_common(10), columns =['ner', 'occurence'])))
-#TODO: id in the list with the df
+# # =============================================================================
+# #  NER counter
+# # =============================================================================
+
+#     #match the paragraphs with corresponding ID
+# regex = r"(\d\n\d+-\d+-\d+\n)(.+)\n(.+\n\n)(.+)"
+# matches = re.finditer(regex, a_strings_transcripts, re.MULTILINE) #beware of generator : needs to be reinitated after use
+# a_matchess = [(match.group(2),match.group(4))  for match in matches] #match ID and text
+# #convert to dic:
+# dict={}
+# for a,b in a_matchess:
+#     dict.setdefault(a, []).append(b)
+# del a,b
+# # explanation: how is it sorted? ; date chronological, despite when you look in the variable explorer (when it's sorted alphabetically)
+
+#     # nlping the latest transcript (ie index 0)
+
+# nlp = spacy.load("en_core_web_sm")
+# doc = nlp(dict[list(dict.keys())[0]][0])
+
+#     #NER summarizing
+# labels, Counter = [ent.label_ for ent in doc.ents], 
+# Counter(labels)
+# items = [ent.text for ent in doc.ents if not (ent.label_ =='CARDINAL')]
+# a_nerCounter = pd.DataFrame(Counter(items).most_common(10), columns =['ner', 'occurence']) #1st argument = list of tuples with a coupleTuple
+#     #now scale to all the transcripts:
+# listOf_a_nerCounter = []
+# for key in list(dict.keys())[:5]:
+#     doc = nlp(dict[key][0])
+#     labels = [ent.label_ for ent in doc.ents]
+#     Counter(labels)
+#     items = [ent.text for ent in doc.ents]
+#     a_nerCounter = pd.DataFrame(Counter(items).most_common(10), columns =['ner', 'occurence']) #1st argument =
+#     listOf_a_nerCounter.append((key, pd.DataFrame(Counter(items).most_common(10), columns =['ner', 'occurence'])))
+# #TODO: id in the list with the df
