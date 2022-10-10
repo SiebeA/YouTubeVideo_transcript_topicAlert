70e23883d2d7e5b16aff9d0293754ea39705d7a1
Siebe albers <savdelz@gmail.com>
2022-10-10 14:32:18 +0300

    Parametrize the following parameters to allow bash execution
    wordOfInterest = 'fake' # Only lowercase will return matches
    book: BOOL # i.e. whether the text involves book or transcript
    transcript_requested

--- Transcript_scraper_NLP_analysis.py
+++ Transcript_scraper_NLP_analysis.py
@@ -11,8 +11,8 @@ def transcript_dic():
         - Frequency NER
         - Corpus: each video is a seperate file, then TFIDF analysis
     """
-    print(os.getcwd())
     os.chdir("/home/siebe/Insync/convexcreate@gmail.com/GD/Engineering/Python/Output")
+    print(os.getcwd(),'\n')
     # os.chdir("G:\\.shortcut-targets-by-id\\1aIirQdvbeZM3DsX_qz7FEEv7LiFQQ9Y3\\Engineering\\Python\Output")
     
     # create a dictionary of all the transcripts-sets
@@ -20,8 +20,25 @@ def transcript_dic():
     for f in glob.glob('*.txt'):
         with open(f, encoding='utf8') as file: 
             transcripts_dic[f] = file.read()
-    del f, file
-    return transcripts_dic
+    # del f, file
+    for key in transcripts_dic.keys(): print(key)
+    
+    
+    transcript_requested = input('\nWhat is the transcript request? [format: firstName_lastname of the author] \n')
+    
+    for string in list(transcripts_dic.keys()):
+        # print(string)
+        if transcript_requested.split('_')[0].lower() in re.split("_| ", string.lower()) or transcript_requested.split('_')[1].lower() in re.split("_| ", string.lower()):
+            print('\n this is the inferred transcript file request:\n\n',string,'\n')
+            transcriptFile_Title_requested = string
+    user_confirmation = input("Enter on the following line whether this is the right transcript txt file? yes/no\n\n")
+    if user_confirmation.lower() != 'yes':
+        # print(transcripts_dic.keys())
+        for key in transcripts_dic.keys(): print(key)
+        transcriptFile_Title_requested = input("\nEnter the name of the transcript txt file on the following line:\n")
+    
+    
+    return transcripts_dic,transcriptFile_Title_requested,transcript_requested
 
 # or when the last script is executed, load the string file:
 
@@ -29,7 +46,7 @@ def transcript_dic():
 '            REGEX return episode where a key word was mentioned:                 '
 #========================= #
 
-def FindAll_wordOfInterest(wordOfInterest:str,transcripts_dic:dict,transcriptFile_requested:str):
+def FindAll_wordOfInterest(transcripts_dic:dict,transcriptFile_Title_requested:str):
     '''
     Parameters
     ----------
@@ -43,17 +60,19 @@ def FindAll_wordOfInterest(wordOfInterest:str,transcripts_dic:dict,transcriptFil
     a_metadata_of_matches : Match_object
         DESCRIPTION.
     '''
+    
+    wordOfInterest = input("Input the wordOfInterest on the following line:\n")
 
     # MATCH the Title etc of the video in which the pattern occurs
     regex = r"(\d{{4}}-\d+-\d+\n.+\n.+)\n\n.+(?={})".format(wordOfInterest) #this regex returns the first match (in first parantheses: title, date, id)c of the 2nd match (2nd parenthesis) 
     
-    a_strings_transcripts = transcripts_dic[transcriptFile_requested]
+    a_strings_transcripts = transcripts_dic[transcriptFile_Title_requested]
     # a_strings_transcripts = a_strings_transcripts.lower() # maximize recall VIDEO ID REQUIRES case sensitive, so links dont work
     # for 1 set of transcripts:
     import re
     a_metadata_of_matches = re.findall(regex, a_strings_transcripts) #it returns the date and title 
     # for _ in a_metadata_of_matches: print(_,'\n')
-    return a_metadata_of_matches,a_strings_transcripts
+    return a_strings_transcripts,wordOfInterest,a_metadata_of_matches
 
 
 
@@ -64,7 +83,7 @@ def FindAll_wordOfInterest(wordOfInterest:str,transcripts_dic:dict,transcriptFil
 # terms = set(words)
 # =============================================================================
 
-def FindAll_contextYwordOfInterest(a_strings_transcripts,chars_of_Context,wordOfInterestψContext):
+def FindAll_contextYwordOfInterest(a_strings_transcripts,wordOfInterest:str,book=False):
     '''
     
 
@@ -83,6 +102,9 @@ def FindAll_contextYwordOfInterest(a_strings_transcripts,chars_of_Context,wordOf
         DESCRIPTION.
 
     '''
+    
+    chars_of_Context = 200
+    
     regex_wordOfInterestψContext = f"(.{chars_of_Context})({wordOfInterest})(.{chars_of_Context})"# matches X chars before and after the word of interest
     regex_wordOfInterestψContext = re.sub('\.(\d+)\)', r'.{\1})', regex_wordOfInterestψContext) # substituting/adding the curly brackets (in)
     if book: # then don't blindly match 200 chars adjoining, but the rest of the paragraph:
@@ -93,10 +115,10 @@ def FindAll_contextYwordOfInterest(a_strings_transcripts,chars_of_Context,wordOf
     a_matches_KeywordψContext = re.findall(regex_wordOfInterestψContext, a_strings_transcripts,flags=re.IGNORECASE|re.MULTILINE)
     # del a_strings_transcripts
     # it APPENDS, so delete file after use, or find a way to not append duplicate lines
-    return a_matches_KeywordψContext
+    return a_matches_KeywordψContext,wordOfInterest,book
 
 #%%
-def Create_SearchResult_File(a_matches_KeywordψContext,book):
+def Create_SearchResult_File(transcript_requested,wordOfInterest,a_matches_KeywordψContext,book):
     """
     
 
@@ -111,17 +133,17 @@ def Create_SearchResult_File(a_matches_KeywordψContext,book):
         DESCRIPTION.
 
     """
-    import os # remove the old output file, otherwise old matches are present
-    output_file = None
-    try:
-        os.remove(output_file.name)
-    except:
-        pass
+    # import os # remove the old output file, otherwise old matches are present
+    # output_file = None
+    # try:
+    #     os.remove(output_file.name)
+    # except:
+    #     pass
     counter = 0
     # error = []
     error = {'SearchPattern':[],'regex':[]}
-    with open(f"SearchResults/SearchRequest{_transcript_requested}+'{wordOfInterest}.txt", "a") as output_file:
-        output_file.truncate(0) # removes all text in the file?
+    with open(f"SearchResults/SearchRequest{transcript_requested}+'{wordOfInterest}.txt", "a") as output_file:
+        output_file.truncate(0) # this removes the CONTENT of the output_file
         for match in a_matches_KeywordψContext:
             try:
                 if book != True: # then we have the metadata in Transcripts
@@ -158,28 +180,17 @@ def Create_SearchResult_File(a_matches_KeywordψContext,book):
 # %%===========================================================================
 # Execution 
 # =============================================================================
-wordOfInterest = 'Depression' # Only lowercase will return matches
+
 words_of_interest = ['rogan', 'fake', 'dutch', ]
-chars_of_Context = 200
-_transcript_requested = 'michael_pollan' # input-format: Firstname_Lastname
-book = True
+case_sensitive = False # TODO
 
 if __name__ == '__main__':
     import re
 
-    transcripts_dic = transcript_dic()
-    for string in list(transcripts_dic.keys()):
-        # print(string)
-        if _transcript_requested.split('_')[0].lower() in re.split("_| ", string.lower()) or _transcript_requested.split('_')[1].lower() in re.split("_| ", string.lower()):
-            print('\n this is the inferred transcript file request:\n\n',string,'\n')
-            transcriptFile_requested = string
-    user_confirmation = input("Enter on the following line whether this is the right transcript txt file? yes/no\n\n")
-    if user_confirmation.lower() != 'yes':
-        print(transcripts_dic.keys())
-        transcriptFile_requested = input("\nEnter the name of the transcript txt file on the following line:\n")
+    transcripts_dic,transcriptFile_Title_requested,transcript_requested = transcript_dic()
         
-    a_matches_KeywordψContext,a_strings_transcripts = FindAll_wordOfInterest(wordOfInterest,transcripts_dic,transcriptFile_requested)
+    a_strings_transcripts,wordOfInterest,a_metadata_of_matches = FindAll_wordOfInterest(transcripts_dic,transcriptFile_Title_requested)
     
-    a_Context_of_matches = FindAll_contextYwordOfInterest(a_strings_transcripts,chars_of_Context,wordOfInterest)
+    a_matches_KeywordψContext,wordOfInterest,book = FindAll_contextYwordOfInterest(a_strings_transcripts,wordOfInterest)
     
-    Create_SearchResult_File(a_Context_of_matches,book)
+    Create_SearchResult_File(transcript_requested,wordOfInterest,a_matches_KeywordψContext,book)
